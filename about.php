b9ec5b7e014508c7c22efe25b6e557ef638720dd
Max <maksym.entex@gmail.com>
2018-06-26 15:07:42 +0300

    init

--- about.php
+++ about.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 24.06.2018
- * Time: 20:50
- */
+$name = 'Макс';
+$age = 27;
+$email = 'maksym.entex@gmail.com';
+$city = 'Севастополь';
+$about = 'менеджер';
+?>
+<!DOCTYPE>
+<html lang="ru">
+<head>
+    <title><?= $name . ' – ' . $about ?></title>
+    <meta charset="utf-8">
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+<h1>Страница пользователя <?= $name ?></h1>
+<dl>
+    <dt>Имя</dt>
+    <dd><?= $name ?></dd>
+    <dt>Возраст</dt>
+    <dd><?= $age ?></dd>
+    <dt>Адрес электронной почты</dt>
+    <dd><a href="mailto:<?= $email ?>"><?= $email ?></a></dd>
+    <dt>Город</dt>
+    <dd><?= $city ?></dd>
+    <dt>О себе</dt>
+    <dd><?= $about ?></dd>
+</dl>
+</body>
+</html>
